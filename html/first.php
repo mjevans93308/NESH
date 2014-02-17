aa4ce2e19ed7dd4c37057d26171d6c5673b56554
aaraadhya <aaraadhya@gmail.com>
2014-02-16 23:09:29 -0800

    all the files on our server right now

---
+++ html/first.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>NESH</title>
+</head>
+
+<body>
+	<p> You are in!</p>
+    <a href="authentication.php?logout">Logout</a>
+</body>
+</html>
