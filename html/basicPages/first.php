4306f0f58cdad3c6f82a37beee59bd3f6c5131f3
aaraadhya <aaraadhya@gmail.com>
2014-02-17 13:18:17 -0800

    fix1

--- html/basicPages/first.php
+++ html/basicPages/first.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="styles/styles.css">
+<link rel="stylesheet" type="text/css" href="../styles/styles.css">
 <title>NESH</title>
 </head>
 
 <body>
 	<p> You are in!</p>
-    <a href="~/php/authentication.php?logout">Logout</a>
+    <a href="../php/authentication.php?logout">Logout</a>
 </body>
 </html>
--- html/php/authentication.php
+++ html/php/authentication.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	include ("~/php/mysqli.php");
+	include ("../php/mysqli.php");
 ?>
 <html>
 <body>
