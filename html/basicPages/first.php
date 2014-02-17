79b50f1e4986ba2e40c8da1e71a2935e2cc8be24
aaraadhya <aaraadhya@gmail.com>
2014-02-17 12:58:30 -0800

    Trying to fix the error of css not being recognized

--- html/basicPages/first.php
+++ html/basicPages/first.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="~/styles/styles.css">
 <title>NESH</title>
 </head>
 
 <body>
 	<p> You are in!</p>
-    <a href="authentication.php?logout">Logout</a>
+    <a href="~/php/authentication.php?logout">Logout</a>
 </body>
 </html>
