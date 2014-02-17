b5fda8d5beb7388bff7699402a09860bd2d2d0f0
mjevans93308 <mevans93308@gmail.com>
2014-02-17 12:42:45 -0800

    committed deleted files [gh-pages]

--- html/mysqli.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_obj = new mysqli("mysql.nesh.co", "bammnesh", "SCUedu11..", "neshco");
-
-if(mysqli_connect_errno())
-{
-	printf("Can't connect. Errorcode: %s\n", mysqli_connect_error());
-	exit;
-}
-?>
