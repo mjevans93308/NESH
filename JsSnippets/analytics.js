494277026d14312a232ae154d1992816d3ad5f38
aaraadhya <aaraadhya@gmail.com>
2014-03-08 21:03:10 -0800

    The CROSS DOMAIN STUFF IS NOW WORKINGGGGGGGGGGGGGGGGG

--- JsSnippets/analytics.js
+++ JsSnippets/analytics.js
@@ -1,52 +1,42 @@
-//<![CDATA[
-    var invocation;
-    var url = 'http://nesh.co/php/getData';
-    var invocationHistoryText;
-    var body = 'key=1, event_id= calcBmi';
-    
-    function callOtherDomain(){
-    	invocation = new XMLHttpRequest();
-        if(invocation)
-        {
-        	alert("Get's into invocation");
-            invocation.open('POST', url, true);
-            invocation.setRequestHeader('X-PINGARUNER', 'pingpong');
-            invocation.setRequestHeader('Content-Type', 'text/html');
-            invocation.onreadystatechange = handler;
-            invocation.send("body"); 
-        }
-        else
-        {
-        	alert("Doesn't get into invocation");
-            invocationHistoryText = "No Invocation TookPlace At All";
-            var textNode = document.createTextNode(invocationHistoryText);
-            var textDiv = document.getElementById("textDiv");
-            alert(textNode);
-        }
-        
-    }
-    function handler(evtXHR)
-    {
-        if (invocation.readyState == 4)
-        {
-                if (invocation.status == 200)
-                {
-                	alert("Gets into readyState");
-                    var response = invocation.responseText;
-                    //var invocationHistory = response.getElementsByTagName('invocationHistory').item(0).firstChild.data;
-                    invocationHistoryText = document.createTextNode(response);
-                    alert(invocationHistoryText);
-                    
-                }
-                else
-                {
-                	alert("Doesn't get into readyState");
-                    alert("Invocation Errors Occured " + invocation.readyState + " and the status is " + invocation.status);
-                }
-        }
-        else
-        {
-            dump("currently the application is at" + invocation.readyState);
-        }
-    }
-    //]]>
+// JavaScript Document
+function createCORSRequest(method, url) {
+	var xhr = new XMLHttpRequest();
+  	if ("withCredentials" in xhr){
+    	xhr.open(method, url, true);
+  	} 
+	else if (typeof XDomainRequest != "undefined") {
+    	xhr = new XDomainRequest();
+    	xhr.open(method, url);
+  	} 
+	else {
+    	xhr = null;
+  	}
+  	return xhr;
+}
+
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+function track(string) {
+	var key = "event_id = '"+string+"'";
+	alert(key);
+	var url = 'http://nesh.co/php/getData.php';
+  	var xhr = createCORSRequest('POST', url);
+  	if (!xhr) {
+    	alert('CORS not supported');
+    	return;
+  	}
+	xhr.onload = function() {
+    	var text = xhr.responseText;
+    	var title = getTitle(text);
+    	alert('Response from CORS request to ' + url + ': ' + title);
+	};
+
+	xhr.onerror = function() {
+		alert('Woops, there was an error making the request.');
+	};
+	alert('Gets here 1!');
+	xhr.send(key);
+	alert('Gets here 2!');
+}
