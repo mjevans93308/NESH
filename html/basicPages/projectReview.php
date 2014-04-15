5ca359965ca602297daacc98f87708ee262aad8f
aaraadhya <aaraadhya@gmail.com>
2014-04-15 08:58:09 -0700

    ProjectReview.php

--- html/basicPages/projectReview.php
+++ html/basicPages/projectReview.php
@@ -34,7 +34,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">NESH</a>
+                <a class="navbar-brand" href="http://nesh.co/basicPages/first.php">NESH</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
