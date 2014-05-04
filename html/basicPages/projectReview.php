29cd35a17e814aafe881dc14dd652145ed3d3386
aaraadhya <aaraadhya@gmail.com>
2014-05-04 15:05:22 -0700

    Trying somwthign out with graphs

--- html/basicPages/projectReview.php
+++ html/basicPages/projectReview.php
@@ -327,33 +327,32 @@
 			//alert("3");
 			document.getElementById("addTagDet1").disabled = true;
 			document.getElementById("addTagOpt").disabled = true;
-			
+			createGraph();
+		}
+		
 			/********************************************************
 									GRAPH AREA
 			*********************************************************/
+		function createGraph(){
 			var r = Raphael(document.getElementById("graph"), document.getElementById("graph").clientWidth, 490),
 			txtattr = { font: "12px sans-serif" };
-                
-                var x = [], y = [], y2 = [], y3 = [];
-
-                for (var i = 0; i < 1e6; i++) {
-                    x[i] = i * 10;
-                    y[i] = (y[i - 1] || 0) + (Math.random() * 7) - 3;
-                    y2[i] = (y2[i - 1] || 150) + (Math.random() * 7) - 3.5;
-                    y3[i] = (y3[i - 1] || 300) + (Math.random() * 7) - 4;
-                }
-				var width = document.getElementById("graph").clientWidth - 20;
-                var lines = r.linechart(20, 0, width, 480, [1, 2, 3, 4, 5, 6, 7],[12, 32, 23, 15, 17, 27, 22], { nostroke: false, axis: "0 0 1 1", symbol: "circle"}).hoverColumn(function () {
-                    this.tags = r.set();
-
-                    for (var i = 0, ii = this.y.length; i < ii; i++) {
-                        this.tags.push(r.tag(this.x, this.y[i], this.values[i], 200, 10).insertBefore(this).attr([{ fill: "#fff" }, { fill: this.symbols[i].attr("fill") }]));
-                    }
-                }, function () {
-                    this.tags && this.tags.remove();
-                });
-
-                lines.symbols.attr({ r: 6 });
+			var x = [], y = [], y2 = [], y3 = [];
+			for (var i = 0; i < 1e6; i++) {
+				x[i] = i * 10;
+				y[i] = (y[i - 1] || 0) + (Math.random() * 7) - 3;
+				y2[i] = (y2[i - 1] || 150) + (Math.random() * 7) - 3.5;
+				y3[i] = (y3[i - 1] || 300) + (Math.random() * 7) - 4;
+			}
+			var width = document.getElementById("graph").clientWidth - 20;
+			var lines = r.linechart(20, 0, width, 480, [1, 2, 3, 4, 5, 6, 7],[12, 32, 23, 15, 17, 27, 22], { nostroke: false, axis: "0 0 1 1", symbol: "circle"}).hoverColumn(function () {
+				this.tags = r.set();
+				for (var i = 0, ii = this.y.length; i < ii; i++) {
+					this.tags.push(r.tag(this.x, this.y[i], this.values[i], 200, 10).insertBefore(this).attr([{ fill: "#fff" }, { fill: this.symbols[i].attr("fill") }]));
+					}
+				}, function () {
+					this.tags && this.tags.remove();
+				});
+				lines.symbols.attr({ r: 6 });
                 // lines.lines[0].animate({"stroke-width": 6}, 1000);
                 // lines.symbols[0].attr({stroke: "#fff"});
                 // lines.symbols[0][1].animate({fill: "#f00"}, 1000);
@@ -386,7 +385,6 @@
 						$("#graph").html("");
 					 });*/
 		}
-		
 		var tag = 1;
 		var tagNum = 1;
 		var postString = '';
@@ -418,6 +416,7 @@
 		function eventSelected(){
 			//alert("onclick works");
 			var eventSelected = $('#eventSelect option:selected').val();
+			postString = '';
 			alert(eventSelected);
 			$('#prepositions').prop('disabled', false);
 			$('#prepositions').selectpicker('refresh');
